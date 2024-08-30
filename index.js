5d0d6621ec0b35e79f93439089dd1a167731ccaf
Harshit Shukla <109151752+HarshitShukla-dev@users.noreply.github.com>
2024-08-30 15:15:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,18 @@
-require('dotenv').config();
 const fs = require('fs');
-const path = require('path');
-const fetch = require('node-fetch');
 
-async function getMembers() {
-  let allMembers = [];
-  let page = 1;
-  let perPage = 100;
-  let members = [];
-  do {
-    const res = await fetch(`https://api.github.com/orgs/LPU-Org/members?per_page=${perPage}&page=${page}`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `token ${process.env.PROFILE_STATS_TOKEN}`
-      }
-    });
-    members = await res.json();
-    allMembers = allMembers.concat(members);
-    page++;
-  } while (members.length === perPage);
-  return allMembers;
-}
+const members = JSON.parse(fs.readFileSync('members.json', 'utf-8'));
 
-async function updateReadme() {
-  const members = await getMembers();
-  let membersTable = '<!-- members -->\n<table><tr>\n';
-  for (let i = 0; i < members.length; i++) {
-    const member = members[i];
-    if (i % 4 === 0 && i !== 0) {
-      membersTable += '</tr>\n<tr>\n';
-    }
-    membersTable += `<td align="center" style="word-wrap: break-word; width: 150.0; height: 150.0">
+const memberList = members.map(member => {
+  return `<td align="center" style="word-wrap: break-word; width: 150.0; height: 150.0">
 <a href="${member.html_url}">
-<img src="${member.avatar_url}" width="100;"  style="border-radius:50%;align-items:center;justify-content:center;overflow:hidden;padding-top:10px" alt="${member.name}"/>
+<img src="${member.avatar_url}" width="100;"  style="border-radius:50%;align-items:center;justify-content:center;overflow:hidden;padding-top:10px" alt="${member.login}"/>
 <br />
 <sub style="font-size:14px"><b>${member.login}</b></sub>
 </a>
-</td>\n`;
-  }
-  membersTable += '</tr>\n</table>\n<!-- endmembers -->';
+</td>`;
+}).join('\n');
 
-  const readme = fs.readFileSync(path.resolve('./README.md'), 'utf-8');
-  const newReadme = readme.replace(/<!-- members -->[\s\S]*<!-- endmembers -->/, membersTable);
-  fs.writeFileSync(path.resolve('./README.md'), newReadme);
-}
+const newReadmeContent = fs.readFileSync('README.md', 'utf-8')
+  .replace(/<!-- members -->[\s\S]*<!-- endmembers -->/, `<!-- members -->\n<table><tr>\n${memberList}\n</tr></table>\n<!-- endmembers -->`);
 
-updateReadme();
+fs.writeFileSync('README.md', newReadmeContent);
